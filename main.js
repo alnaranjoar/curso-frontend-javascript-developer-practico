67ca9825fc71009aff2b4a1ca829a2b85e2b4ceb
Alejandro Naranjo <anaranjoa93@gmail.com>
2023-06-23 17:55:39 -0500

    Fusión en index.html del menu versión mobile. Cambios en js y css de acuerdo con este cambio

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burgerMenu = document.querySelector('.burger-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 navEmail.addEventListener('click', toggleDesktopMenu)
+burgerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive') //toggle activa o inactiva dependiendo del estado. 
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
 
