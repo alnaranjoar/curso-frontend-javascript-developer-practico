71d89801f041406a15ad01e220fb22d1ab741f74
Alejandro Naranjo <anaranjoa93@gmail.com>
2023-07-06 17:59:00 -0500

    Se pueden filtrar los productos por categoria, solo aparecen en pantalla los que corresponden a la categoría seleccionada

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ const mobileMenu = document.querySelector('.mobile-menu')
 const desktopShopingCartImg = document.querySelector('.navbar-shopping-cart')
 const desktopShoppingCart = document.querySelector('.shopping-cart-container')
 
+const desktopAll = document.getElementById('desktop-menu-all')
+const desktopClothes = document.getElementById('desktop-menu-clothes')
+const desktopElectronics = document.getElementById('desktop-menu-electronics')
+const desktopFurnitures = document.getElementById('desktop-menu-furnitures')
+const desktopToys = document.getElementById('desktop-menu-toys')
+const desktopOthers = document.getElementById('desktop-menu-others')
+
 //product detail aside definitions
 const productDetailContainer = document.querySelector('.product-detail')
 const productDetailCloseIcon = document.querySelector('.product-detail-close')
@@ -23,25 +30,37 @@ burgerMenu.addEventListener('click', toggleMobileMenu)
 desktopShopingCartImg.addEventListener('click', toggleShoppingCart)
 productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
+desktopAll.addEventListener('click',function(){productsFilter(products)})
+desktopClothes.addEventListener('click',function(){productsFilter(clothes)})
+desktopElectronics.addEventListener('click',function(){productsFilter(electronics)})
+desktopFurnitures.addEventListener('click',function(){productsFilter(furniture)})
+desktopToys.addEventListener('click',function(){productsFilter(toys)})
+desktopOthers.addEventListener('click',function(){productsFilter(others)})
 
 class Product {
-    constructor(name,price,image,description){
+    constructor(name,price,image,description,type){
         this.name = name
         this.price = price
         this.image = image
         this.description = description
+        this.type = type
     }
 }
 
-let bike = new Product('Bike', 120, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.')
+let bike = new Product('Bike', 120, 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.', 'furniture')
 
-let light = new Product('Light', 10, 'https://contents.mediadecathlon.com/p1898422/k$641c8d34be572289d162705f9090300d/luces-para-bicicleta-led-st-920-delantero-slash-trasero-usb.jpg?format=auto&quality=40&f=452x452', 'This lights help you see at night and also cars and motorcycles can always see you')
+let light = new Product('Light', 10, 'https://contents.mediadecathlon.com/p1898422/k$641c8d34be572289d162705f9090300d/luces-para-bicicleta-led-st-920-delantero-slash-trasero-usb.jpg?format=auto&quality=40&f=452x452', 'This lights help you see at night and also cars and motorcycles can always see you','electronics')
 
-let helmet = new Product('Helmet', 40, 'https://cdn.shopify.com/s/files/1/0604/4637/6123/products/659436676916.jpg?v=1640799457', 'Protect your head from damage with a high quality helmet')
+let helmet = new Product('Helmet', 40, 'https://cdn.shopify.com/s/files/1/0604/4637/6123/products/659436676916.jpg?v=1640799457', 'Protect your head from damage with a high quality helmet', 'toys')
 
 let products = []
-
 products.push(bike, light, helmet)
+
+let clothes = products.filter((product) => product.type == 'clothes')
+let electronics = products.filter((product) => product.type == 'electronics')
+let furniture = products.filter((product) => product.type == 'furniture')
+let toys = products.filter((product) => product.type == 'toys')
+let others = products.filter((product) => product.type == 'others')
 
 //Desktop menu
 function toggleDesktopMenu() {
@@ -89,7 +108,7 @@ function toggleShoppingCart() {
 
  function changeProductDetailAsideInfo(productName) {
 
-     products.forEach(product => {
+     products.forEach((product) => {
          if(productName == product.name) {
              productDetailImage.setAttribute('src', product.image)
              productDetailPrice.innerHTML = '$' + product.price
@@ -127,7 +146,7 @@ function closeProductDetailAside() {
 function renderProducts(array) {
     for(product of array) {
         const productCard = `
-        <div class="product-card">
+        <div class="product-card ${product.type}">
             <img class="product-card-image" src="${product.image}" alt="" onclick="changeProductDetailAsideInfo('${product.name}')">
             <div class="product-info">
                 <div>
@@ -144,5 +163,12 @@ function renderProducts(array) {
     }
 }
 
+function productsFilter(array) {
+
+    cardsContainer.innerHTML = ""
+
+    renderProducts(array)
+
+}
 renderProducts(products)
 
